b7fbcd5a7f0f6781e600ef4dd880ed5027a6a897
Ubaid-Ali <syedubaidali2020@gmail.com>
2020-12-08 15:18:10 +0500

    added jest library and enzyme added App folder with it's 3 files added Timer folder with it's 3 files

---
+++ src/Components/Timer/Timer.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+const Timer = () => {
+
+  function startTimer() {
+    console.log('Starting timer.');
+  }
+
+  function stopTimer() {
+    console.log('Stopping timer.');
+  }
+
+  function resetTimer() {
+    console.log('Resetting timer.');
+  }
+
+  return (
+    <div className="timer-container">
+
+    </div>
+  )
+}
+
+export default Timer;
